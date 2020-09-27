8fbbc075fe125f30e5590456813ee3f3c22b7928
Simon Merrick <simonmerrick@catalyst.net.nz>
2020-09-28 11:25:00 +1300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+__version__ = "1.0"
+
+setup(
+    name="synapse-swift-storage-provider",
+    version=__version__,
+    zip_safe=False,
+    url="https://github.com/iokiwi/synapse-swift-storage-provider",
+    py_modules=["swift_storage_provider"],
+    scripts=["scripts/swift_media_upload"],
+    install_requires=[
+        # "boto3>=1.9.23<2.0",
+        # "botocore>=1.12.23<2.0",
+        "openstacksdk>=0.50.0",
+        "humanize>=0.5.1<0.6",
+        "psycopg2>=2.7.5<3.0",
+        "PyYAML>=3.13<4.0",
+        "tqdm>=4.26.0<5.0",
+    ],
+)
---
+++ swift_storage_provider.py
@@ -0,0 +1,359 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 New Vector Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import os
+import threading
+
+from six import string_types
+
+# import boto3
+# import botocore
+import openstack
+
+from twisted.internet import defer, reactor
+from twisted.python.failure import Failure
+from twisted.python.threadpool import ThreadPool
+
+from synapse.logging.context import LoggingContext, make_deferred_yieldable
+from synapse.rest.media.v1._base import Responder
+from synapse.rest.media.v1.storage_provider import StorageProvider
+
+# Synapse 1.13.0 moved current_context to a module-level function.
+try:
+    from synapse.logging.context import current_context
+except ImportError:
+    current_context = LoggingContext.current_context
+
+logger = logging.getLogger("synapse.swift")
+
+
+# The list of valid AWS storage class names
+# _VALID_STORAGE_CLASSES = (
+#     "STANDARD",
+#     "REDUCED_REDUNDANCY",
+#     "STANDARD_IA",
+#     "INTELLIGENT_TIERING",
+# )
+
+# Chunk size to use when reading from swift connection in bytes
+READ_CHUNK_SIZE = 16 * 1024
+
+
+class SwiftStorageProviderBackend(StorageProvider):
+    """
+    Args:
+        hs (HomeServer)
+        config: The config returned by `parse_config`
+    """
+
+    def __init__(self, hs, config):
+        self.cache_directory = hs.config.media_store_path
+        self.container = config["container"]
+        # self.storage_class = config["storage_class"]
+        self.api_kwargs = {}
+
+        if "os_project_name" in config:
+            self.api_kwargs["project_name"] = config["os_project_name"]
+
+        if "os_region_name" in config:
+            self.api_kwargs["region_name"] = config["os_region_name"]
+
+        if "os_auth_url" in config:
+            self.api_kwargs["auth_url"] = config["os_auth_url"]
+
+        if "os_username" in config:
+            self.api_kwargs["username"] = config["os_username"]
+
+        if "os_password" in config:
+            self.api_kwargs["password"] = config["os_password"]
+
+        if "os_user_domain_name" in config:
+            self.api_kwargs["user_domain_name"] = config["os_user_domain_name"]
+
+        # TODO(simon): These should default to 'Default' or 'default' unless
+        # overidden
+        if "os_user_domain_name" in config:
+            self.api_kwargs["user_domain_name"] = config["os_user_domain_name"]
+
+        if "os_project_domain_name" in config:
+            self.api_kwargs["project_domain_name"] = config["os_project_domain_name"]
+
+        threadpool_size = config.get("threadpool_size", 40)
+        self._download_pool = ThreadPool(
+            name="swift-download-pool", maxthreads=threadpool_size
+        )
+        self._download_pool.start()
+
+    def store_file(self, path, file_info):
+        """See StorageProvider.store_file"""
+
+        def _store_file():
+            connection = openstack.connect(**self.api_kwargs)
+            connection.object_store.create_object(
+                self.container,
+                path,
+                filename=os.path.join(self.cache_directory, path)
+            )
+
+        # XXX: reactor.callInThread doesn't return anything, so I don't think this does
+        # what the author intended.
+        return make_deferred_yieldable(reactor.callInThread(_store_file))
+
+    def fetch(self, path, file_info):
+        """See StorageProvider.fetch"""
+        logcontext = current_context()
+
+        d = defer.Deferred()
+        self._download_pool.callInThread(
+            swift_download_task, self.container, self.api_kwargs, path, d, logcontext
+        )
+        return make_deferred_yieldable(d)
+
+    @staticmethod
+    def parse_config(config):
+        """Called on startup to parse config supplied. This should parse
+        the config and raise if there is a problem.
+
+        The returned value is passed into the constructor.
+
+        In this case we return a dict with fields, `bucket` and `storage_class`
+        """
+
+        container = config["container"]
+
+        assert isinstance(container, string_types)
+
+        result = {
+            "container": container,
+        }
+
+
+        if "os_project_name" in config:
+            result["project_name"] = config["os_project_name"]
+
+        if "os_region_name" in config:
+            result["region_name"] = config["os_region_name"]
+
+        if "os_auth_url" in config:
+           result["auth_url"] = config["os_auth_url"]
+
+        if "os_username" in config:
+            result["username"] = config["os_username"]
+
+        if "os_password" in config:
+            result["password"] = config["os_password"]
+
+        if "os_user_domain_name" in config:
+            result["user_domain_name"] = config["os_user_domain_name"]
+
+        if "os_project_domain_name" in config:
+            result["project_domain_name"] = config["os_project_domain_name"]
+
+        return result
+
+
+def swift_download_task(container, api_kwargs, object_name, deferred, parent_logcontext):
+    """Attempts to download a file from swift.
+
+    Args:
+        container (str): The swift bucket which may have the file
+        api_kwargs (dict): Keyword arguments to pass when invoking the API.
+            Generally `os_auth_url`.
+        object_name (str): The object_name of the file
+        deferred (Deferred[_SwiftResponder|None]): If file exists
+            resolved with an _SwiftResponder instance, if it doesn't
+            exist then resolves with None.
+        parent_logcontext (LoggingContext): the logcontext to report logs and metrics
+            against.
+    """
+    with LoggingContext(parent_context=parent_logcontext):
+        logger.info("Fetching %s from swift", object_name)
+
+        local_data = threading.local()
+
+        try:
+            connection = local_data.connection
+        except AttributeError:
+            connection = openstack.connect(**api_kwargs)
+            local_data.connection = connection
+
+        try:
+            resp = connection.download_object(object_name, container)
+        except openstack.exceptions.ResourceNotFound as e:
+                logger.info("Media %s not found in swift", key)
+                reactor.callFromThread(deferred.callback, None)
+                return
+
+            reactor.callFromThread(deferred.errback, Failure())
+            return
+
+        producer = _SwiftResponder()
+        reactor.callFromThread(deferred.callback, producer)
+        _stream_to_producer(reactor, producer, resp["Body"], timeout=90.0)
+
+
+def _stream_to_producer(reactor, producer, body, status=None, timeout=None):
+    """Streams a file like object to the producer.
+
+    Correctly handles producer being paused/resumed/stopped.
+
+    Args:
+        reactor
+        producer (_SwiftResponder): Producer object to stream results to
+        body (file like): The object to read from
+        status (_ProducerStatus|None): Used to track whether we're currently
+            paused or not. Used for testing
+        timeout (float|None): Timeout in seconds to wait for consume to resume
+            after being paused
+    """
+
+    # Set when we should be producing, cleared when we are paused
+    wakeup_event = producer.wakeup_event
+
+    # Set if we should stop producing forever
+    stop_event = producer.stop_event
+
+    if not status:
+        status = _ProducerStatus()
+
+    try:
+        while not stop_event.is_set():
+            # We wait for the producer to signal that the consumer wants
+            # more data (or we should abort)
+            if not wakeup_event.is_set():
+                status.set_paused(True)
+                ret = wakeup_event.wait(timeout)
+                if not ret:
+                    raise Exception("Timed out waiting to resume")
+                status.set_paused(False)
+
+            # Check if we were woken up so that we abort the download
+            if stop_event.is_set():
+                return
+
+            chunk = body.read(READ_CHUNK_SIZE)
+            if not chunk:
+                return
+
+            reactor.callFromThread(producer._write, chunk)
+
+    except Exception:
+        reactor.callFromThread(producer._error, Failure())
+    finally:
+        reactor.callFromThread(producer._finish)
+        if body:
+            body.close()
+
+class _SwiftResponder(Responder):
+    """A Responder for swift. Created by _SwiftDownloadThread
+    """
+
+    def __init__(self):
+        # Triggered by responder when more data has been requested (or
+        # stop_event has been triggered)
+        self.wakeup_event = threading.Event()
+        # Trigered by responder when we should abort the download.
+        self.stop_event = threading.Event()
+
+        # The consumer we're registered to
+        self.consumer = None
+
+        # The deferred returned by write_to_consumer, which should resolve when
+        # all the data has been written (or there has been a fatal error).
+        self.deferred = defer.Deferred()
+
+    def write_to_consumer(self, consumer):
+        """See Responder.write_to_consumer
+        """
+        self.consumer = consumer
+        # We are a IPushProducer, so we start producing immediately until we
+        # get a pauseProducing or stopProducing
+        consumer.registerProducer(self, True)
+        self.wakeup_event.set()
+        return make_deferred_yieldable(self.deferred)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop_event.set()
+        self.wakeup_event.set()
+
+    def resumeProducing(self):
+        """See IPushProducer.resumeProducing
+        """
+        # The consumer is asking for more data, signal _SwDownloadThread
+        self.wakeup_event.set()
+
+    def pauseProducing(self):
+        """See IPushProducer.stopProducing
+        """
+        self.wakeup_event.clear()
+
+    def stopProducing(self):
+        """See IPushProducer.stopProducing
+        """
+        # The consumer wants no more data ever, signal _S3DownloadThread
+        self.stop_event.set()
+        self.wakeup_event.set()
+        if not self.deferred.called:
+            with LoggingContext():
+                self.deferred.errback(Exception("Consumer ask to stop producing"))
+
+    def _write(self, chunk):
+        """Writes the chunk of data to consumer. Called by _S3DownloadThread.
+        """
+        if self.consumer and not self.stop_event.is_set():
+            self.consumer.write(chunk)
+
+    def _error(self, failure):
+        """Called when a fatal error occured while getting data. Called by
+        _S3DownloadThread.
+        """
+        if self.consumer:
+            self.consumer.unregisterProducer()
+            self.consumer = None
+
+        if not self.deferred.called:
+            self.deferred.errback(failure)
+
+    def _finish(self):
+        """Called when there is no more data to write. Called by _S3DownloadThread.
+        """
+        if self.consumer:
+            self.consumer.unregisterProducer()
+            self.consumer = None
+
+        if not self.deferred.called:
+            self.deferred.callback(None)
+
+
+class _ProducerStatus(object):
+    """Used to track whether the swift download thread is currently paused
+    waiting for consumer to resume. Used for testing.
+    """
+
+    def __init__(self):
+        self.is_paused = threading.Event()
+        self.is_paused.clear()
+
+    def wait_until_paused(self, timeout=None):
+        is_paused = self.is_paused.wait(timeout)
+        if not is_paused:
+            raise Exception("Timed out waiting")
+
+    def set_paused(self, paused):
+        if paused:
+            self.is_paused.set()
+        else:
+            self.is_paused.clear()
