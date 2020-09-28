df2a29ca13c45b2d2cac90832d9bfd10fe17f867
Simon Merrick <simon.merrick@outlook.co.nz>
2020-09-29 08:30:18 +1300

    Just drop python 3.5 test

--- setup.py
+++ setup.py
@@ -10,11 +10,10 @@ setup(
     py_modules=["swift_storage_provider"],
     scripts=["scripts/swift_media_upload"],
     install_requires=[
-        "openstacksdk>=0.47.0,<=0.47.0; python_version='3.5'",
+        "openstacksdk>=0.50.0",
         "humanize>=0.5.1<0.6",
         "psycopg2>=2.7.5<3.0",
         "PyYAML>=3.13<4.0",
         "tqdm>=4.26.0<5.0",
-        "dogpile.cache<=1.0.1; python_version='3.5'"
     ],
 )
